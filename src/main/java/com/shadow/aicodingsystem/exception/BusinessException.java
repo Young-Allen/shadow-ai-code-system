95642e8b85f13b14218e27d332b21eb91d885c58
WXD <wxd020513@163.com>
2026-01-11 17:00:53 +0800

    test

--- src/main/java/com/shadow/aicodingsystem/exception/BusinessException.java
+++ src/main/java/com/shadow/aicodingsystem/exception/BusinessException.java
@@ -1,7 +1,23 @@
 package com.shadow.aicodingsystem.exception;
 
 public class BusinessException extends RuntimeException {
-  public BusinessException(String message) {
+  /**
+   * 错误码
+   */
+  private final int code;
+
+  public BusinessException(int code, String message) {
     super(message);
+    this.code = code;
+  }
+
+  public BusinessException(ErrorCode errorCode , String message) {
+    super(message);
+    this.code = errorCode.getCode();
+  }
+
+  public BusinessException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.code = errorCode.getCode();
   }
 }
