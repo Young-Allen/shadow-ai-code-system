b6fcd83d4e4e0f0821b367c4e19fa1e1a85ac973
WXD <wxd020513@163.com>
2026-01-11 16:02:51 +0800

    # This is a combination of 3 commits. # This is the 1st commit message:
    add BusinessException and ThrowUtils
    
    # This is the commit message #2:
    
    test commit
    
    # This is the commit message #3:
    
    refactor
    
    test

--- src/main/java/com/shadow/aicodingsystem/exception/ThrowUtils.java
+++ src/main/java/com/shadow/aicodingsystem/exception/ThrowUtils.java
@@ -1,4 +1,35 @@
 package com.shadow.aicodingsystem.exception;
 
 public class ThrowUtils {
+    /**
+     * 条件成立则抛异常
+     * @param condition
+     * @param exception
+     */
+    public static void throwIf(boolean condition, RuntimeException exception) {
+        if (condition) {
+            throw exception;
+        }
+    }
+
+    /**
+    * 条件成立则抛异常
+    * @param condition
+    * @param errorCode
+    */
+    public static void throwIf(boolean condition, ErrorCode errorCode) {
+        throwIf(condition, new BusinessException(errorCode));
+    }
+
+
+    /**
+     * 条件成立则抛异常
+     * @param condition
+     * @param errorCode
+     * @param message
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode, String message) {
+        throwIf(condition, new BusinessException(errorCode, message));
+    }
+
 }
