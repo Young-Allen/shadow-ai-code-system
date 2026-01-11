0c43ec0e54f39f4a7350a60743e1b9b2e48d9025
WXD <wxd020513@163.com>
2026-01-11 16:02:51 +0800

    add BusinessException and ThrowUtils

--- src/main/java/com/shadow/aicodingsystem/exception/BusinessException.java
+++ src/main/java/com/shadow/aicodingsystem/exception/BusinessException.java
@@ -1,7 +1,24 @@
 package com.shadow.aicodingsystem.exception;
 
 public class BusinessException extends RuntimeException {
-  public BusinessException(String message) {
-    super(message);
-  }
+    /**
+     * 错误码
+     * @param message
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
 }
